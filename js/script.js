7bbc322fd21d1bce697225d7396fea862a8a61fa
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2020-01-29 10:36:06 +0200

    feat:add responsiveness

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const input = document.querySelector('input');
 const area = document.getElementById('displayer');
+const pageBorder = document.getElementById('container')
 
 input.addEventListener('input', () => {
     const currColor = input.value;
 
-    area.style.backgroundColor = currColor;                                                                                                                                                 olor = currColor;
+    area.style.backgroundColor = currColor;
+    pageBorder.style.border = '2px solid' + currColor;                                                                                                                                                 olor = currColor;
     document.getElementById('hex').innerText = currColor;
 });
